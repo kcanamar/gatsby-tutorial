458b4235d62f2027c3634b41af65229f0850054b
Kyle Canamar <canamar.kyle@gmail.com>
2022-08-03 10:29:59 -0700

    completed part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,16 +2,19 @@ import * as React from 'react'
 import Layout from '../components/layout'
 // Step 1: Import the graphql tag
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 // Step 2: Export a page query
 export const query = graphql`
-query {
-    allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
-      edges {
-        node {
-          id
-          name
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
         }
+        id
+        body
       }
     }
   }
@@ -24,10 +27,12 @@ export default function BlogPage({ data }){
       { /* Step 3: Use the data in your component*/ }
       <ul>
         {
-            data.allFile.edges.map((edge, index) => (
-                <li key={index}>
-                    {edge.node.name}
-                </li>
+            data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <MDXRenderer>{node.body}</MDXRenderer>
+                </article>
             ))
         }
       </ul>
